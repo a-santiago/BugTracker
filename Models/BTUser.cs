86d2a83f0f7b8af5404ed4dc656cdcfb66145b61
a-santiago <santiago.62596@gmail.com>
2021-08-24 02:30:20 -0400

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        
+        
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+
+
+        [NotMapped]
+        [DisplayName("Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
+
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
+    }
+}
